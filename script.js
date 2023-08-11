59367af6cc9d7026b5d7eeb3cbb301c6d20f9741
Dima <ten.dima10@yandex.ru>
2023-08-12 01:57:47 +0600

    упростил js код

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 let startDay = document.querySelector('.start-date').value;
 let endDay = document.querySelector('.end-date').value;
-let left = new Date(endDay) - new Date();
+let leftTime = new Date(endDay) - new Date();
 
 function foo() {
     let month = document.querySelector('.mo');
     let day = document.querySelector('.d');
     let hour = document.querySelector('.h');
 
-    let m = Math.floor(left / 1000 / 60 / 60 / 24 / 30);
-    let d = Math.floor(left / 1000 / 60 / 60 / 24 % 30);;
-    let h = Math.floor(left / 1000 / 60 / 60 % 24);; 
-    let min = Math.floor(left / 1000 / 60 % 60);
-    let sec = left % 60;
+    let m = Math.floor(leftTime / 1000 / 60 / 60 / 24 / 30);
+    let d = Math.floor(leftTime / 1000 / 60 / 60 / 24 % 30);;
+    let h = Math.floor(leftTime / 1000 / 60 / 60 % 24);; 
+    let min = Math.floor(leftTime / 1000 / 60 % 60);
+    let sec = leftTime % 60;
 
     sec = sec < 10 ? '0' + sec : sec;
     min = min < 10 ? '0' + min : min;
@@ -22,7 +22,7 @@ function foo() {
     day.textContent = d;
     hour.innerHTML = `${h}:${min}:${sec}`;
 
-    left--;
+    leftTime--;
 }
 
 setInterval(foo, 1000);
@@ -65,22 +65,19 @@ drowDays();
 
 function drowDays() {
     let days = document.querySelector('.daysMatrix');
-    let today = new Date() - new Date(startDay);
-    
+    let daysToDrow = (new Date(endDay) - new Date(startDay)) / 1000 / 60 / 60 / 24;
 
-
-    for (let i = 0; i < left / 1000 / 60 / 60 / 24; i++) {
+    for (let i = 0; i < daysToDrow; i++) {
         let day = document.createElement('div');
         day.classList.add('dayMatrix');
         days.append(day);
     }
 
-    let a = Math.round((left - today) / 1000 / 60 / 60 / 24);
-    let b = left / 1000 / 60 / 60 / 24;
+    let today = (new Date() - new Date(startDay)) / 1000 / 60 / 60 / 24;
     let div = document.querySelectorAll('.dayMatrix');
-    for (let i = 0; i < (b - a); i++) {
-        div[i].classList.add('dayPass')
 
+    for (let i = 0; i < today; i++) {
+        div[i].classList.add('dayPass')
     }
 }
 
